76260f0381fc628987bea86484159c72a9fc7646
dongshujin <dongshujin.beans@gmail.com>
2023-09-24 11:38:35 +0800

    Initial Commit

---
+++ XHomeLibrary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XHomeLibrary
+//
+//  Created by dongshujin on 2023/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XHomeLibrary/XHomeLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  XHomeLibraryApp.swift
+//  XHomeLibrary
+//
+//  Created by dongshujin on 2023/9/24.
+//
+
+import SwiftUI
+
+@main
+struct XHomeLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
